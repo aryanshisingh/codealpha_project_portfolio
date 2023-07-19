9fddd3b35fd0fea1dba43bcc8c93ce1fc36b3c5b
Aryanshi Singh <136102177+aryanshisingh@users.noreply.github.com>
2023-07-19 20:33:16 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,18 @@
+
+const sr=ScrollReveal({
+    distance:'35px',
+    duration:2400,
+    reset:true
+});
+sr.reveal('.logo',{delay:210,origin:'left'});   
+sr.reveal('.navbar',{delay:310,origin:'top'});   
+sr.reveal('.top-btnn',{delay:410,origin:'right'});
+
+sr.reveal('.main-content h4',{delay:510,origin:'top'});
+sr.reveal('.main-content h1',{delay:610,origin:'left'});
+sr.reveal('.main-content p',{delay:710,origin:'right'});
+sr.reveal('.main-content h1',{delay:810,origin:'left'});
+
+
+
+
